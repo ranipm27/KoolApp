797f77b7804d23c5fc2258eb80fde6f09c514f15
Dipak Dutta <dipak.dutta@shoes.com>
2019-12-03 17:50:01 +0530

    Initial Commit

---
+++ src/main/java/com/ibm/containercrush/shoes/KoolAppApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.containercrush.shoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoolAppApplication.class, args);
+	}
+
+}
