690731e8c043bd165177efe7baad3500f2052991
Dipak Dutta <dipak.dutta@shoes.com>
2019-12-03 20:44:38 +0530

    added view

--- src/main/java/com/ibm/containercrush/shoes/KoolAppApplication.java
+++ src/main/java/com/ibm/containercrush/shoes/KoolAppApplication.java
@@ -2,12 +2,20 @@ package com.ibm.containercrush.shoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class KoolAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KoolAppApplication.class, args);
 	}
 
+	@RequestMapping("index")
+	public String index()
+	{
+		return "index.html";
+	}
 }
